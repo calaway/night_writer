e783eb5dbb77832fc575434c5065adc0fe25c1f4
Calaway <Christopher.Calaway@gmail.com>
2016-07-13 13:00:00 -0600

    nondescript updates

--- runner.rb
+++ runner.rb
@@ -2,4 +2,4 @@ require './lib/plaintext'
 
 
 plaintext_message_1 = Plaintext.new("helloworld")
-puts plaintext_message_1.translate_plaintext_to_braille
+puts plaintext_message_1.translate_plaintext_to_braille.inspect
